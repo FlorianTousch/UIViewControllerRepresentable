4ce37dc342d452f6d97588fd46a435bff21e6bb1
FlorianTo <florian.tousch@hotmail.fr>
2022-02-03 10:41:13 +0100

    Initial Commit

---
+++ UIViewControllerRepresentable/UIViewControllerRepresentableApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewControllerRepresentableApp.swift
+//  UIViewControllerRepresentable
+//
+//  Created by Florian on 03/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct UIViewControllerRepresentableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
